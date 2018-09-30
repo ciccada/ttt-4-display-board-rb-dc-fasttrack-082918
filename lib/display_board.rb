cd2e16bc85349678624de007ebdc3456afbc0ab4
Zachari Curtis <zacharijlease@gmail.com>
2018-09-30 14:14:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 # out the current state.
 
 def display_board(board)
- puts "#{board[0]}|#{broard[1]}|#{board[2]}"
+ puts "#{board[0]}|#{board[1]}|#{board[2]}"
  puts "-----------"
  puts "#{board[3]}|#{board[4]}|#{board[5]}"
  puts "-----------"
