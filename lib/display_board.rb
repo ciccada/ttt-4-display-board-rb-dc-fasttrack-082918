08ae14ae4da62948e94ec160816b116b9328a4eb
Zachari Curtis <zacharijlease@gmail.com>
2018-09-30 14:08:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 
 def display_board(board)
   if board = [" "," "," "," "," "," "," "," "," "]
- puts "   |   |   "
+ puts "#{board[0]}|   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
