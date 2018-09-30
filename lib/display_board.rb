c1b317806a6df9b76bb0dc03db144fa4f8b4c978
Zachari Curtis <zacharijlease@gmail.com>
2018-09-30 13:56:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,10 @@
 # out the current state.
 def display_board(board)
   if board = [" "," "," "," "," "," "," "," "," "]
- puts expect(rows[0]).to eq("   |   |   ")
- puts    expect(rows[1]).to eq("-----------")
- puts     expect(rows[2]).to eq("   |   |   ")
- puts    expect(rows[3]).to eq("-----------")
- puts     expect(rows[4]).to eq("   |   |   ") 
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   " 
+ end
 end
